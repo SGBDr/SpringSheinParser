911741c1fdffced8c897b8b55b45c517962b0ce7
Kengoum Gassam <kengoumgassam@gmail.com>
2021-11-29 08:49:32 +0100

    new config

--- src/main/java/com/example/demo/Result/ResultService.java
+++ src/main/java/com/example/demo/Result/ResultService.java
@@ -40,7 +40,7 @@ public class ResultService {
 				}
 				resultList.add(new Result(image, price, name, link));
 			}
-			if(resultList.size() == 100)break;
+			if(resultList.size() == 50)break;
 		}
 		return resultList;
 	}
